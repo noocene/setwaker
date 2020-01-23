b8531881c146f76c0927f669067369aa3c883c45
Izzy Swart <zenerboson@gmail.com>
2020-01-23 11:24:41 -0800

    add Clone to SetWaker

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ use lock_api::{Mutex, RawMutex};
 #[cfg(not(feature = "no_std"))]
 use std::{collections::HashSet, sync::Arc};
 
+#[derive(Clone)]
 pub struct SetWaker<M: RawMutex, K> {
     inner: WakerPointer<M, K>,
 }
